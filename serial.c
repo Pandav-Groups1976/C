0e68f428dcd73e4178a92db66d5c40383c266100
Pandav-Groups1976 <68810818+Pandav-Groups1976@users.noreply.github.com>
2021-04-04 21:33:47 +0530

    using while loop in c

--- serial.c
+++ serial.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    for(a=1;a<=10;a++)
-    {
-     printf("%d,",11-a);
-    }
+    int a=123,rem,sum=0;
+   while(a!=0)
+   {
+       rem = a%10;
+       sum=sum*10 + rem;
+
+       a=a/10;
+       //printf("%d,",rem);
+
+   }
+   printf("%d",sum);
     return 0;
 }
