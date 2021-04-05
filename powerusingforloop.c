ad86a9eec6a82ffaca497ce82d4f6b5bda1869fc
Pandav-Groups1976 <68810818+Pandav-Groups1976@users.noreply.github.com>
2021-04-05 13:31:27 +0530

    powerusingforloop

---
+++ powerusingforloop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,i,pow=1;
+    printf("Enter the number");
+    scanf("%d",&x);
+    printf("Enter the power");
+    scanf("%d",&y);
+    for(i=1;i<=y;i++)
+    {
+    pow=pow*x;
+    }
+    printf("%d",pow);
+    return 0;
+
+
+}
