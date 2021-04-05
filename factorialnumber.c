f8b54b21a616b0170acec2c9f97295c667ba29ef
Pandav-Groups1976 <68810818+Pandav-Groups1976@users.noreply.github.com>
2021-04-05 13:45:21 +0530

    finding factorial number using for loop

---
+++ factorialnumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num=5,fact=1,i;
+    for(i=1;i<=num;i++)
+    {
+        fact=fact*i;
+
+    }
+printf("%d",fact);
+    return 0;
+
+}
--- powerusingforloop.c
+++ powerusingforloop.c
@@ -13,5 +13,4 @@ int main()
     printf("%d",pow);
     return 0;
 
-
 }
