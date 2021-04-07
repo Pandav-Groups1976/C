805a06b255df4daf1aed669bffabd029db85c208
Pandav-Groups1976 <68810818+Pandav-Groups1976@users.noreply.github.com>
2021-04-07 19:24:51 +0530

    Triangle Shape

---
+++ armstrongnumberusingwhileloop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int m=8;
+int n=3;
+    int i,j;
+     for(i=1;i<=m;i++)//Outer loop
+     {
+
+         for(j=1;j<=i;j++)//Inner loop
+         {
+            // printf("i=%4d,j=%4d",i,j);
+            printf("%d",0);
+         }
+         printf("\n");
+         }
+
+    return 0;
+}
