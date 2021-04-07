b963d72ccea1a01c386111d12973938a0d95f398
Pandav-Groups1976 <68810818+Pandav-Groups1976@users.noreply.github.com>
2021-04-07 19:29:35 +0530

    Right shifted Triangle

--- armstrongnumberusingwhileloop.c
+++ armstrongnumberusingwhileloop.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int m=8;
-int n=3;
-    int i,j;
-     for(i=1;i<=m;i++)//Outer loop
+    //int m=8;
+int n=7;
+    int i,j,k;
+     for(i=1;i<=n;i++)//Outer loop
      {
+           for(j=1;j<=n-i;j++)//Inner loop
+         {
+            // printf("i=%4d,j=%4d",i,j);
+            printf(" ");
+         }
 
-         for(j=1;j<=i;j++)//Inner loop
+         for(k=1;k<=i;k++)//Inner loop
          {
             // printf("i=%4d,j=%4d",i,j);
             printf("%d",0);
