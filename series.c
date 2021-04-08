b687033b46f30479a1b5e2222753069815d51251
Pandav-Groups1976 <68810818+Pandav-Groups1976@users.noreply.github.com>
2021-04-08 14:56:35 +0530

    printing series of even number

--- series.c
+++ series.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int n=4;
+    int n=12;
     int i,j,k;
      for(i=n;i>=1;i--)
+
      {
            for(j=1;j<=n-i;j++)
          {
@@ -13,9 +14,11 @@ int main()
 
          for(k=1;k<=2*i-1;k++)
          {
+             if(k%2==0)
 
-            printf("%d",k);
+            printf("%d,",k);
          }
+
          printf("\n");
          }
 
